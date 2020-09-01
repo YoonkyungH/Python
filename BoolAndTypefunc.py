98090b123e836513c7f9fc54b81ba3b00073f8fd
yoonkyung <ykha9911@gmail.com>
2020-09-01 18:37:40 +0900

    boolean and typefunction

---
+++ BoolAndTypefunc.py
@@ -0,0 +1,27 @@
+# boolean
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)
+print(not not True)
+print(not not False)
+
+print(2>1)
+print(2==2)
+print(5<=1)
+
+print(2>1 and "Hello"=="Hello")
+print(7==7 or (4<3 and 12>10))
+
+# type 함수 : ()의 type을 알려주는 함수
+print(type(3))
+print(type(3.0))
+print(type("3"))
