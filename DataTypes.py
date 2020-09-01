5e6c197b0df1f3d1d10dfb27b2ca1c848f1b061b
yoonkyung <ykha9911@gmail.com>
2020-09-01 17:54:36 +0900

    DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,16 @@
+# 거듭제곱
+print(2 ** 3)
+
+# floor division (버림 나눗셈) : 소수 부분을 버림
+print(7 // 2)
+print(8.0 // 3)
+
+# round (반올림)
+print(round(3.1415926535, 2))
+
+# 문자열
+print("큰 따옴표")
+print('작은 따옴표')
+print("I\'m \"excited\" to learn Python!")
+print("Hello" + "World")
+print("Hello" * 3)
