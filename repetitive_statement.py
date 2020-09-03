79d8a6d9df8b820766ea19bd94d2c25965de2c6c
yoonkyung <ykha9911@gmail.com>
2020-09-03 14:09:52 +0900

    for

---
+++ repetitive_statement.py
@@ -0,0 +1,17 @@
+# for문 기초
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+print()
+
+# range 함수 (간편, 편리, 메모리 효율성)
+for i in range(3, 11):  # 주의사항 : 3~10까지임
+    print(i)
+print()
+
+for i in range(10):  # 주의사항 : 0~9까지임
+    print(i)
+print()
+
+for i in range(3, 17, 3):  # 3부터 16까지 3 간격으로
+    print(i)
+print()
