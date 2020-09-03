fd29a04d885d6651ab10ea27b1fb55607bc2b58d
yoonkyung <ykha9911@gmail.com>
2020-09-03 13:31:42 +0900

    list

--- list_prac.py
+++ list_prac.py
@@ -55,3 +55,33 @@ while i < len(prices):
     i += 1
 
 print("일본 화폐: " + str(prices))
+print()
+
+
+# 리스트 함수 활용하기
+numbers = []
+print(numbers)
+
+numbers.append(1)
+numbers.append(7)
+numbers.append(3)
+numbers.append(6)
+numbers.append(5)
+numbers.append(2)
+numbers.append(13)
+numbers.append(14)
+print(numbers)
+
+i = 0
+while i < len(numbers):
+    if numbers[i] % 2 == 1:
+        del numbers[i]
+    else:
+        i += 1
+print(numbers)
+
+numbers.insert(0, 20)
+print(numbers)
+
+numbers.sort()
+print(numbers)
