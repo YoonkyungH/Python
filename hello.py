dd3a096a0e065155baf177b8a6e4e9435938e44a
yoonkyung <ykha9911@gmail.com>
2020-09-04 22:16:46 +0900

    for

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
 print('hello world')
+# 코멘트
---
+++ repetitive_statement_prac.py
@@ -0,0 +1,13 @@
+# range 연습
+numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+for i in range(len(numbers)):
+    print(i, numbers[i])
+print()
+
+
+# 거듭제곱
+for i in range(11):
+    print("{}^{} = {}".format(2, i, 2 ** i))
+
+print()
