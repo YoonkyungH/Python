01e8eac47cd52721e7eac16be6b67c1d3a47e5ec
yoonkyung <ykha9911@gmail.com>
2020-09-05 22:59:13 +0900

    for

--- repetitive_statement_prac.py
+++ repetitive_statement_prac.py
@@ -11,3 +11,9 @@ for i in range(11):
     print("{}^{} = {}".format(2, i, 2 ** i))
 
 print()
+
+
+# for문으로 구구단
+for i in range(1, 10):  # 주의사항 : 3~10까지임
+    for j in range(1, 10):
+        print("{} * {} = {}".format(i, j, i * j))
