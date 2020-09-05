f49254ffdadcad330f161769815877c5a4ec4834
yoonkyung <ykha9911@gmail.com>
2020-09-05 23:09:07 +0900

    for

--- repetitive_statement_prac.py
+++ repetitive_statement_prac.py
@@ -17,3 +17,15 @@ print()
 for i in range(1, 10):  # 주의사항 : 3~10까지임
     for j in range(1, 10):
         print("{} * {} = {}".format(i, j, i * j))
+
+print()
+
+
+# 피타고라스 삼조
+for a in range(1, 1000):
+    for b in range(1, 1000):
+        c = 1000 - a - b
+        if a * a + b * b == c * c and a < b < c:
+            print(a * b * c)
+
+print()
