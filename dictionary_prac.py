6553806fc36a085816ec5593c49c5e77cd6202e3
yoonkyung <ykha9911@gmail.com>
2020-09-07 21:20:06 +0900

    dictionary

---
+++ dictionary_prac.py
@@ -0,0 +1,12 @@
+# 영어 단어장
+vocab = {
+    "sanitizer": "살균제",
+    "ambition": "야망",
+    "conscience": "양심",
+    "civilization": "문명"
+}
+print(vocab)
+vocab["privilege"] = "특권"
+vocab["principle"] = "원칙"
+print(vocab)
+
