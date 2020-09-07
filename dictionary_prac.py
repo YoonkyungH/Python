54749c72e0f660514cc703b39799c5016d1979a9
yoonkyung <ykha9911@gmail.com>
2020-09-07 22:33:35 +0900

    dictionary

--- dictionary_prac.py
+++ dictionary_prac.py
@@ -40,4 +40,26 @@ print("영-한 단어장\n{}\n".format(vocab))
 
 # 변환된 단어장 출력
 reversed_vocab = reverse_dict(vocab)
-print("한-영 단어장\n{}".format(reversed_vocab))
+print("한-영 단어장\n{}\n".format(reversed_vocab))
+
+
+
+# 투표 집계하기
+# 투표 결과 리스트
+votes = ['김영자', '강승기', '최만수', '김영자', '강승기', '강승기', '최만수', '김영자', \
+'최만수', '김영자', '최만수', '김영자', '김영자', '최만수', '최만수', '최만수', '강승기', \
+'강승기', '김영자', '김영자', '최만수', '김영자', '김영자', '강승기', '김영자']
+
+# 후보별 득표수 사전
+vote_counter = {}
+
+# 리스트 votes를 이용해서 사전 vote_counter를 정리하기
+for name in votes:
+    if name not in vote_counter:
+        vote_counter[name] = 1
+    else:
+        vote_counter[name] += 1
+
+
+# 후보별 득표수 출력
+print(vote_counter)
